5c7ee7de424343414dc4fba7e5150ec374a9e7f9
xFA25E <55886566+xFA25E@users.noreply.github.com>
2019-12-14 17:29:55 +0100

    Solved part one of day 2

--- advent-of-code-2019.scm
+++ advent-of-code-2019.scm
@@ -20,7 +20,7 @@
       acc
       (sum-fuels (cdr masses) (+ acc (fn (car masses))) fn)))
 
-(define (day-1-a) (sum-fuels modules-masses 0 calc-fuel))
+(define (day-1-a) (sum-fuels modules-masses 0 fuel-for-mass))
 
 (define (fuel-for-fuel fuel acc)
   (let ((fl (fuel-for-mass fuel)))
@@ -33,3 +33,29 @@
              (lambda (m)
                (let ((fuel-mass (fuel-for-mass m)))
                  (+ fuel-mass (fuel-for-fuel fuel-mass 0))))))
+
+(define (computer-parse program)
+  (apply vector (map string->number (string-split program #\,))))
+
+(define (computer-run program pc)
+  (let ((elm (vector-ref program pc)))
+    (if (= elm 99)
+        program
+        (let* ((fn (case elm ((1) +) ((2) *)))
+               (left-op (vector-ref program (+ pc 1)))
+               (right-op (vector-ref program (+ pc 2)))
+               (store-addr (vector-ref program (+ pc 3))))
+          (vector-set! program store-addr
+                       (fn (vector-ref program left-op)
+                           (vector-ref program right-op)))
+          (computer-run program (+ pc 4))))))
+
+(define (computer-exec program)
+  (let ((program (computer-parse program)))
+    (vector-set! program 1 12)
+    (vector-set! program 2 2)
+    (computer-run program 0)))
+
+(define computer-program (call-with-input-file "2.inputs.txt" read-line))
+
+(define (day-2-a) (vector-ref (computer-exec computer-program) 0))
