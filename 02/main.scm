8696d83226821cbb52232176cae5885479500dec
xFA25E <55886566+xFA25E@users.noreply.github.com>
2019-12-14 18:40:12 +0100

    Solved day 2

--- 02/main.scm
+++ 02/main.scm
@@ -20,16 +20,53 @@
                            (vector-ref program right-op)))
           (computer-run program (+ pc 4))))))
 
-(define (computer-exec program)
-  (let ((program (computer-parse program)))
-    (vector-set! program 1 12)
-    (vector-set! program 2 2)
-    (computer-run program 0)))
-
 (define computer-program (call-with-input-file input-file read-line))
 
-(define (part-one) (vector-ref (computer-exec computer-program) 0))
+(define (set-inputs program pos-1 pos-2)
+  (vector-set! program 1 pos-1)
+  (vector-set! program 2 pos-2)
+  program)
+
+(define (part-one)
+  (let ((program (computer-parse computer-program)))
+    (vector-ref (computer-run (set-inputs program 12 2) 0) 0)))
+
+(define (generate-inputs)
+  (define (inner-1 n b acc)
+    (if (<= 0 n)
+        (inner-1 (1- n) b (cons (cons n b) acc))
+        acc))
+
+  (define (inner-2 n acc)
+    (if (<= 0 n)
+        (inner-2 (1- n) (inner-1 99 n acc))
+        acc))
+
+  (inner-2 99 '()))
+
+(define part-two-result 19690720)
+
+(define (part-two)
+  (let ((inputs (generate-inputs)))
+    (letrec ((inner
+              (lambda (acc)
+                (if (null? acc)
+                    'not-found
+                    (let ((a (caar acc))
+                          (b (cdar acc))
+                          (program (computer-parse computer-program)))
+                      (if (= part-two-result
+                             (vector-ref
+                              (computer-run (set-inputs program a b) 0) 0))
+                          (cons a b)
+                          (inner (cdr acc))))))))
+      (let ((result (inner inputs)))
+        (+ (cdr result) (* 100 (car result)))))))
 
 (display "Part one: ")
 (display (part-one))
+(newline)
+
+(display "Part two: ")
+(display (part-two))
 (newline)
